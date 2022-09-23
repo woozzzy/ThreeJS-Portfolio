4c60c61a2d8d44896fee0d73f8104cd10b0c28c0
Sunwoo Park <sunwooid@gmail.com>
2022-09-22 22:20:56 -0400

    Add avatar shape. Add moon shape. Add template html. Add Scroll animation. Add CSS Styling.

--- src/main.ts
+++ src/main.ts
@@ -63,9 +63,50 @@ const addStar = () => {
 
 Array(200).fill(0).forEach(addStar);
 
+// Add Avatar Shape
+const avatarTexture = new THREE.TextureLoader().load("/src/assets/avatar.jpg");
+const avatar = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshBasicMaterial({ map: avatarTexture })
+);
+
+scene.add(avatar);
+
+// Add Moon Shape
+const moonTexture = new THREE.TextureLoader().load("/src/assets/moon.jpg");
+const normalTexture = new THREE.TextureLoader().load("/src/assets/normal.jpg");
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+    normalMap: normalTexture,
+  })
+);
+
+scene.add(moon);
+
+moon.position.z = 30;
+moon.position.x = -30;
+
 // Load background
 const bgTexture = new THREE.TextureLoader().load("/src/assets/background.jpg");
 scene.background = bgTexture;
+
+const moveCamera = () => {
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  avatar.rotation.y += 0.01;
+  avatar.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+};
+
+document.body.onscroll = moveCamera;
 
 // Animation Loop
 const animate = () => {
